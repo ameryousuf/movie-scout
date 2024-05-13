c32d6642ac490c631295d476b7d21f0a6c09bd05
Amer Yousuf <amer.yousuf.am@gmail.com>
2024-05-13 15:37:26 +0100

    feat: Implement theme toggle functionality

--- modules/header/src/lib/theme-toggle/theme-toggle.component.ts
+++ modules/header/src/lib/theme-toggle/theme-toggle.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
@@ -13,9 +13,49 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ThemeToggleComponent {
+  private readonly document = inject(DOCUMENT);
+  private readonly themeLinkId = 'theme';
+
   protected darkTheme = true;
 
   toggleTheme() {
-    // TODO: Implement theme toggling logic
+    if (this.darkTheme) {
+      this.enableLightTheme();
+    } else {
+      this.disableLightTheme();
+    }
+
+    this.darkTheme = !this.darkTheme;
+  }
+
+  private enableLightTheme() {
+    this.getThemeLinkElement().setAttribute('href', 'light-theme.css');
+  }
+
+  private disableLightTheme() {
+    const existingLinkElement = this.getExistingThemeLinkElement();
+    if (existingLinkElement) {
+      this.document.head.removeChild(existingLinkElement);
+    }
+  }
+
+  private getThemeLinkElement() {
+    return (
+      this.getExistingThemeLinkElement() || this.createThemeLinkElement()
+    );
+  }
+
+  private getExistingThemeLinkElement() {
+    return this.document.head.querySelector(
+      `link[rel="stylesheet"]#${this.themeLinkId}`,
+    );
+  }
+
+  private createThemeLinkElement() {
+    const linkEl = this.document.createElement('link');
+    linkEl.setAttribute('rel', 'stylesheet');
+    linkEl.id = this.themeLinkId;
+    this.document.head.appendChild(linkEl);
+    return linkEl;
   }
 }
